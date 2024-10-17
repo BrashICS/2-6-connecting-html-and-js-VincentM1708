a97637579a937fa191849eff71b1f41f076e350f
Vincent Moli <vincent.molinier@stu.ocsb.ca>
2024-10-17 10:42:58 -0400

    useful functions and eventlisteners

--- main.js
+++ main.js
@@ -9,12 +9,55 @@
 'use strict';
 
 /*** Event Listeners ***/
+document.getElementById("d6").addEventListener("click", roll_d6)
+document.getElementById("d8").addEventListener("click", roll_d8)
 
+document.getElementById("d6").addEventListener("mouseenter", d6_enter)
+document.getElementById("d6").addEventListener("mouseleave", d6_leave)
+
+
+document.getElementById("enter").addEventListener("click", greeting);
 
 /*** Functions ***/
+function roll_d6() {
+    let roll = randInt(1, 6)
+    document.getElementById("die_roll").textContent = roll;
+}
+
+function roll_d8(){
+    let d8_roll = randInt(1, 8)
+    document.getElementById("die_roll").textContent = d8_roll;
+}
+
+
+
+
+function d6_enter(){
+    document.getElementById("die").textContent = "🤯"
+
+}
+
+
+function d6_leave(){
+    document.getElementById("die").textContent = "🎲"
+
+}
+
+
+function greeting(){
+    let name = document.getElementById("user_input").value;
+    document.getElementById("greeting").textContent = ("Hello" + " " + name)
+
+}
+
+
+
 
 // Return a random integer from `min` to `max`, inclusive
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
 
